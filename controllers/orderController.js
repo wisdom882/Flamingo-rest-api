95b79895d71d5df4b6a448710a79d7fc13f18640
wisdom <miabiyewisdom@gmail.com>
2021-04-28 15:49:16 -0700

    routesAdded

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,8 @@
 
 //get order by id
 
+//get orders
+
 //delete order
 
 import asyncHandler from 'express-async-handler'
@@ -24,6 +26,36 @@ const addOrder = asyncHandler(async(req, res) => {
         user: req.user_id
     })
 
-    await order.save()
+    const newOrder = await order.save()
 
+    res.status(201).json(newOrder)
+
+})
+
+const deleteOrder = asyncHandler((res, req) => {
+  
+    console.log(req.body)
+
+    //check if cart is empty or invalid
+    if(items && items.length === 0)
+    {
+        res.status(400)
+        throw new Error('no items in order')   
+    }
+
+    const order = new Order({
+
+        items : items
+        
+    })
+
+    const deletedOrder = await order.delete()
+
+    res.status(201).json(deletedOrder)
+})
+
+const getOrders = asyncHandler((req,res) => {
+
+    // Can you give me a hint on how this function works?
+    //like what does it return?s
 })
