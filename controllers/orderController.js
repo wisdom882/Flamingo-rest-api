f80743da59f9ad54fda046fa4c77fb9baf772934
wisdom <miabiyewisdom@gmail.com>
2021-05-03 10:53:56 -0700

    OrderRoutesandController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -21,4 +21,41 @@ const addOrders = asyncHandler(async (req, res) => {
   res.status(201).json(newOrder);
 });
 
-export { addOrders };
+const deleteOrder = asyncHandler(async(req,res) => {
+    console.log(req.body)
+
+    const orderToDelete = await Order.findById(req.params.id)
+
+    if(orderToDelete)
+    {
+      orderToDelete.delete()
+      res.status({message:"order successfully deleted"})
+    }
+    else{
+      res.status(404)
+      throw new Error("Order could not be deleted")
+    }
+
+})
+
+const getOrders = asyncHandler(async(res,req) => {
+  console.log(req.body)
+  const orders = await Order.find({}) 
+  res.json(orders)
+
+})
+
+const getOrderById = asyncHandler(async(res,req) => {
+  console.log(req.body)
+  const orderById = await Order.findById(req.params.id)
+
+  if(orderById)
+  {
+    res.json(orderById)
+  }
+  else{
+    res.status(404)
+    throw new Error("Could not find order")
+  }
+})
+export { addOrders, deleteOrder, getOrders, getOrderById };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { addOrders } from "../controllers/orderController.js";
+import {
+    addOrders,
+    getOrder,
+    getOrderById,
+    deleteOrder
+} from '../controllers/orderController.js'
+
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, addOrders);
+
+router
+  .route("/:id")
+  .get(protect, getOrderById)
+  .get(protect, getOrder)
+  .delete(protect, deleteOrder);
 
 export default router;
